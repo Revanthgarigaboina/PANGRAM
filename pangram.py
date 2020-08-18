d439a0acdd744d7d26c1cee93108d115c2e735d6
Revanthgarigaboina <67013670+Revanthgarigaboina@users.noreply.github.com>
2020-08-18 19:18:02 +0530

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -1,11 +1,18 @@
-def ispangram(str):
+def pangram(str):
     alphabet=("abcdefghijklmnopqrstuvwxyz")
     for char in alphabet:
         if char not in str.lower():
             return False
     return True
 string=input("please enter the sentence")
-if(ispangram(string)==True):
-    print("the input is a pangram")
+if(pangram(string)==True):
+    print("the given input is a pangram")
 else:
     print("the given input is not a pangram")
+Input:
+      Please enter the sentence
+      the quick brown fox jumps over the lazy dog
+Output:
+      the given input is a pangram
+       
+      
