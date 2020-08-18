82a56112115340d10f671e9ee43364b36f911fd7
Revanthgarigaboina <67013670+Revanthgarigaboina@users.noreply.github.com>
2020-08-18 19:19:51 +0530

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -9,10 +9,6 @@ if(pangram(string)==True):
     print("the given input is a pangram")
 else:
     print("the given input is not a pangram")
-Input:
-      Please enter the sentence
-      the quick brown fox jumps over the lazy dog
-Output:
-      the given input is a pangram
+
        
       
